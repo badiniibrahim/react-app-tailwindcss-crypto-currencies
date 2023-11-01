d7856a42268e3a0a54014706befa0c7a628e3cf5
Ibrahimovic <ibrahimovic@MacBook-Air-de-Ibrahimovic.local>
2023-11-01 18:33:31 +0100

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
+    build: { manifest: true, outDir: "./dist" },
+    base: "/",
+    root: "./src",
     define: {
       "process.env": env,
     },
